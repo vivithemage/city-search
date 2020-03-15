0cf34ca787af5d6dcd0a6ad775430058d95e278b
RS <rs@mage.me.uk>
2020-03-15 15:23:25 +0000

    Add ICityFinder and ICityResult interfaces

---
+++ ICityFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace City_Search
+{
+    interface ICityFinder
+    {
+        ICityResult Search(string searchString);
+    }
+}
---
+++ ICityResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace City_Search
+{
+    interface ICityResult
+    {
+        ICollection<string> NextLetters { get; set; }
+        ICollection<string> NextCities { get; set; }
+    }
+}
