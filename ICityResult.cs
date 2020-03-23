44349cacdc5af2630f1f4e1240002eff340ce385
RS <rs@mage.me.uk>
2020-03-23 21:43:43 +0000

    Create class that implements ICityResult

--- ICityResult.cs
+++ ICityResult.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace City_Search
 {
-    interface ICityResult
+    public interface ICityResult
     {
         ICollection<string> NextLetters { get; set; }
         ICollection<string> NextCities { get; set; }
+    }
+
+    public class CityResult : ICityResult
+    {
+        private ICollection<string> nextCharacters = new List<string>();
+        private ICollection<string> nextCities = new List<string>();
+
+        public ICollection<string> NextLetters
+        {
+            get
+            {
+                return nextCharacters;
+            }
+            set
+            {
+                nextCharacters = value;
+            }
+                  
+        }
+
+        public ICollection<string> NextCities
+        {            
+            get
+            {
+                return nextCities;
+            }
+
+            set
+            {
+                nextCities = value;
+            }
+        }
     }
 }
